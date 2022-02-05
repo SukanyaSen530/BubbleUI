336a31f1f1194a5addc049f032f6b03b1f2268ff
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-02-05 21:28:53 +0530

    feat - added alerts

--- scripts/doc.js
+++ scripts/doc.js
@@ -4,3 +4,17 @@ const sidebar = document.querySelector("#sidebar");
 hamburgerBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+
+// DEMO FOR ALERT
+
+demoAlertCloseBtn = document.querySelector("#demo-alert .alert-cancel");
+demoAlert = document.querySelector("#demo-alert");
+showAlert = document.querySelector("#alert-demo-undoBtn");
+
+demoAlertCloseBtn.addEventListener("click", () => {
+  demoAlert.style.visibility = "hidden";
+});
+
+showAlert.addEventListener("click", () => {
+  demoAlert.style.visibility = "visible";
+});
