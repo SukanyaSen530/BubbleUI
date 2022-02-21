028649c4cd16e4be88e63625b870cb5d287aea35
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2022-02-21 12:51:56 +0530

    feat - added dark theme

--- scripts/doc.js
+++ scripts/doc.js
@@ -39,3 +39,46 @@ resposiveNavbarHamburger.addEventListener("click", () => {
   console.log(navbarResponsive);
   navbarResponsive.classList.toggle("active");
 });
+
+// THEME
+
+const lightTheme = () => {
+  document.documentElement.style.setProperty("--white", "#fff");
+  document.documentElement.style.setProperty("--dark", "black");
+  document.documentElement.style.setProperty("--slate-600", "#475569");
+  document.documentElement.style.setProperty(
+    "--box-shadow-dark",
+    "rgb(0 0 0 / 15%) 0px 2px 8px"
+  );
+};
+
+const darkTheme = () => {
+  document.documentElement.style.setProperty("--white", "#212529");
+  document.documentElement.style.setProperty("--dark", "whitesmoke");
+  document.documentElement.style.setProperty("--slate-600", "#fff");
+  document.documentElement.style.setProperty(
+    "--box-shadow-dark",
+    "rgb(255 255 255 / 40%) 0px 2px 8px"
+  );
+};
+
+const changeTheme = () => {
+  if (JSON.parse(localStorage.getItem("darkTheme"))) {
+    darkTheme();
+  } else {
+    lightTheme();
+  }
+};
+
+const indexThemeBtn = document.getElementById("docs-theme-button");
+indexThemeBtn.addEventListener("click", () => {
+  if (window.localStorage.getItem("darkTheme")) {
+    let theme = JSON.parse(window.localStorage.getItem("darkTheme"));
+    window.localStorage.setItem("darkTheme", !theme);
+  } else {
+    window.localStorage.setItem("darkTheme", true);
+  }
+  changeTheme();
+});
+
+changeTheme();
